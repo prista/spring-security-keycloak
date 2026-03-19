e9ad180fe49abf7dd642f2df9e2bb8ed46bb3118
pritsa <drmhere@icloud.com>
2026-03-19 12:59:22 +0300

    #basic auth

---
+++ src/main/java/com/drm/sandbox/security/security/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.drm.sandbox.security.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(authorize ->
+                        authorize.anyRequest().authenticated())
+                .build();
+    }
+}
