11488c53f63811e55f49c8a13650cb0e15fe80bd
pritsa <drmhere@icloud.com>
2026-03-19 12:09:12 +0300

    #initialzr

---
+++ src/test/java/com/drm/sandbox/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drm.sandbox.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
