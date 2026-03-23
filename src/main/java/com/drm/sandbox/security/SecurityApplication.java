70c9c3fcd70c3d5f38d07270d11262d074d06eaa
pritsa <drmhere@icloud.com>
2026-03-23 16:55:55 +0300

    #part2 - get info about the auth user (functional approach)

--- src/main/java/com/drm/sandbox/security/SecurityApplication.java
+++ src/main/java/com/drm/sandbox/security/SecurityApplication.java
@@ -2,12 +2,37 @@ package com.drm.sandbox.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.RouterFunctions;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import java.util.Map;
 
 @SpringBootApplication
 public class SecurityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityApplication.class, args);
+	}
+
+	@Bean
+	public RouterFunction<ServerResponse> routerFunction() {
+		return RouterFunctions.route()
+				.GET("/api/v4/greetings", request -> {
+					var userDetails = request.principal().map(Authentication.class::cast)
+							.map(Authentication::getPrincipal)
+							.map(UserDetails.class::cast)
+							.orElseThrow();
+					return ServerResponse.ok()
+							.contentType(MediaType.APPLICATION_JSON)
+							.body(Map.of("greeting", "Hello V4, %s!".formatted(userDetails.getUsername())));
+
+				})
+				.build();
 	}
 
 }
