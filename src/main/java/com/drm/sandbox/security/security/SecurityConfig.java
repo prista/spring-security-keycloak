31056420905d5672dead3bbe73fc624e0b3de0ec
pritsa <drmhere@icloud.com>
2026-03-27 17:57:13 +0300

    #override default auth entry point logic

--- src/main/java/com/drm/sandbox/security/security/SecurityConfig.java
+++ src/main/java/com/drm/sandbox/security/security/SecurityConfig.java
@@ -2,19 +2,26 @@ package com.drm.sandbox.security.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-//@Configuration
-//public class SecurityConfig {
-//
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        return http
-//                .httpBasic(Customizer.withDefaults())
-//                .authorizeHttpRequests(authorize ->
-//                        authorize.anyRequest().authenticated())
-//                .build();
-//    }
-//}
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                //.httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(authorize ->
+                        authorize.anyRequest().authenticated())
+                .exceptionHandling(exceptionHandling -> exceptionHandling
+                        .authenticationEntryPoint((request, response, authException) -> {
+                                    authException.printStackTrace();
+                                    response.sendError(HttpStatus.UNAUTHORIZED.value());
+                                }
+                        ))
+                .build();
+    }
+}
