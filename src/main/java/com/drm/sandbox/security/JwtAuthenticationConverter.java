c9da0f2577f25166803501a16867950e1f9760a3
pritsa <drmhere@icloud.com>
2026-04-18 12:06:41 +0300

    #JwtAuthenticationConverter

---
+++ src/main/java/com/drm/sandbox/security/JwtAuthenticationConverter.java
@@ -0,0 +1,41 @@
+package com.drm.sandbox.security;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.jspecify.annotations.Nullable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationConverter;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
+
+import java.util.function.Function;
+
+public class JwtAuthenticationConverter implements AuthenticationConverter {
+
+    private final Function<String, Token> accessTokenStringDeserializer;
+    private final Function<String, Token> refreshTokenStringDeserializer;
+
+    public JwtAuthenticationConverter(Function<String, Token> accessTokenStringDeserializer,
+                                      Function<String, Token> refreshTokenStringDeserializer) {
+        this.accessTokenStringDeserializer = accessTokenStringDeserializer;
+        this.refreshTokenStringDeserializer = refreshTokenStringDeserializer;
+    }
+
+    @Override
+    public @Nullable Authentication convert(HttpServletRequest request) {
+        var authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            var token = authHeader.replace("Bearer ", "");
+
+            var accessToken = accessTokenStringDeserializer.apply(token);
+            if (accessToken != null) {
+                return new PreAuthenticatedAuthenticationToken(accessToken, token);
+            }
+            // if not an access token, try to parse as refresh token
+            var refreshToken = refreshTokenStringDeserializer.apply(token);
+            if (refreshToken != null) {
+                return new PreAuthenticatedAuthenticationToken(refreshToken, token);
+            }
+        }
+        return null;
+    }
+}
